ca8062af4a8b0996e18a0c011c4a0b00568ef2f9
cedkechat01 <mathiskera2509@gmail.com>
2026-08-28 22:12:40 +0200

    Optimizing the itoa function

--- src/lib/string.c
+++ src/lib/string.c
@@ -72,42 +72,41 @@ bool strneq(const char* string_1, const char* string_2, u32 limit) {
 }
 
 // function made by cedkechat01
-char* itoa(u32 num, char* string_1, u32 size) {
-    // end of string pointer
-    int index = size - 1;
-    string_1[index] = '\0';
+char *itoa(u32 num, char *string_1, u32 base) {
+    u32 i = 0;
 
-    // handle zero case
-    if (num == 0) {
-        index = index - 1;
-        string_1[index] = '0';
-        return &string_1[index];
-    }
-
-    // unsigned types cant be negative
     bool negative = false;
 
-    if (num < 0) {
+    if (num == 0) {
+        string_1[i++] = '0';
+        string_1[i] = '\0';
+        return string_1;
+    }
+
+    if (num < 0 && base == 10) {
         negative = true;
         num = -num;
     }
 
-    // extract digits right-to-left
-    while (num > 0) {
-        u32 digit = num % 10; //get last digit
-        index = index - 1;    // move left in buffer
-        string_1[index] = '0' + digit; // convert to ASCII
-        num = num / 10;       // remove last digit
+    while (num != 0) {
+        int rem = num % base;
+        string_1[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
     }
 
-    // add minus sign if needed
-    if (negative == true) {
-        index = index - 1;
-        string_1[index] = '-';
+    if (negative == true) string_1[i++] = '-';
+
+    string_1[i] = '\0';
+
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        swap((string_1 + start), (string_1 + end));
+        start++;
+        end--;
     }
 
-    // return start of string
-    return &string_1[index];
+    return string_1;
 }
 
 u32 atoi(const char *s) {
